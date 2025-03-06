5cf845a40e0ed4866c537e87de25269aff159577
Joseph <josamudiamen1@gmail.com>
2025-03-06 02:36:39 +0100

    Fix app startup for GCP deployment

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template
 from app import create_app
+import os
 
+# Create the Flask application
 app = create_app()
 
+# This is used when running the app locally
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)), debug=False) 
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import create_app
+import os
 
+# Create the Flask application
 app = create_app()
 
+# This is used when running the app locally
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True) 
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)), debug=False) 
